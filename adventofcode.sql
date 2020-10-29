28b6a914a4862ac9e13e1221feffaa8adc533a03
Kraystone <leo.catifait@epsi.fr>
2020-10-29 11:57:39 +0100

    database

---
+++ adventofcode.sql
@@ -0,0 +1,288 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  mar. 01 sep. 2020 à 15:42
+-- Version du serveur :  10.4.8-MariaDB
+-- Version de PHP :  7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `adventofcode`
+--
+CREATE DATABASE IF NOT EXISTS `adventofcode` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `adventofcode`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `2015`
+--
+
+CREATE TABLE IF NOT EXISTS `2015` (
+  `dateJour` int(11) NOT NULL,
+  `statut` tinyint(1) NOT NULL,
+  PRIMARY KEY (`dateJour`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `2015`
+--
+
+INSERT INTO `2015` (`dateJour`, `statut`) VALUES
+(1, 0),
+(2, 0),
+(3, 0),
+(4, 0),
+(5, 0),
+(6, 0),
+(7, 0),
+(8, 0),
+(9, 0),
+(10, 0),
+(11, 0),
+(12, 0),
+(13, 0),
+(14, 0),
+(15, 0),
+(16, 0),
+(17, 0),
+(18, 0),
+(19, 0),
+(20, 0),
+(21, 0),
+(22, 0),
+(23, 0),
+(24, 0),
+(25, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `2016`
+--
+
+CREATE TABLE IF NOT EXISTS `2016` (
+  `dateJour` int(11) NOT NULL,
+  `statut` tinyint(1) NOT NULL,
+  PRIMARY KEY (`dateJour`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `2016`
+--
+
+INSERT INTO `2016` (`dateJour`, `statut`) VALUES
+(1, 0),
+(2, 0),
+(3, 0),
+(4, 0),
+(5, 0),
+(6, 0),
+(7, 0),
+(8, 0),
+(9, 0),
+(10, 0),
+(11, 0),
+(12, 0),
+(13, 0),
+(14, 0),
+(15, 0),
+(16, 0),
+(17, 0),
+(18, 0),
+(19, 0),
+(20, 0),
+(21, 0),
+(22, 0),
+(23, 0),
+(24, 0),
+(25, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `2017`
+--
+
+CREATE TABLE IF NOT EXISTS `2017` (
+  `dateJour` int(11) NOT NULL,
+  `statut` tinyint(1) NOT NULL,
+  PRIMARY KEY (`dateJour`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `2017`
+--
+
+INSERT INTO `2017` (`dateJour`, `statut`) VALUES
+(1, 0),
+(2, 0),
+(3, 0),
+(4, 0),
+(5, 0),
+(6, 0),
+(7, 0),
+(8, 0),
+(9, 0),
+(10, 0),
+(11, 0),
+(12, 0),
+(13, 0),
+(14, 0),
+(15, 0),
+(16, 0),
+(17, 0),
+(18, 0),
+(19, 0),
+(20, 0),
+(21, 0),
+(22, 0),
+(23, 0),
+(24, 0),
+(25, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `2018`
+--
+
+CREATE TABLE IF NOT EXISTS `2018` (
+  `dateJour` int(11) NOT NULL,
+  `statut` tinyint(1) NOT NULL,
+  PRIMARY KEY (`dateJour`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `2018`
+--
+
+INSERT INTO `2018` (`dateJour`, `statut`) VALUES
+(1, 0),
+(2, 0),
+(3, 0),
+(4, 0),
+(5, 0),
+(6, 0),
+(7, 0),
+(8, 0),
+(9, 0),
+(10, 0),
+(11, 0),
+(12, 0),
+(13, 0),
+(14, 0),
+(15, 0),
+(16, 0),
+(17, 0),
+(18, 0),
+(19, 0),
+(20, 0),
+(21, 0),
+(22, 0),
+(23, 0),
+(24, 0),
+(25, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `2019`
+--
+
+CREATE TABLE IF NOT EXISTS `2019` (
+  `dateJour` int(11) NOT NULL,
+  `statut` tinyint(1) NOT NULL,
+  PRIMARY KEY (`dateJour`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `2019`
+--
+
+INSERT INTO `2019` (`dateJour`, `statut`) VALUES
+(1, 0),
+(2, 0),
+(3, 0),
+(4, 0),
+(5, 0),
+(6, 0),
+(7, 0),
+(8, 0),
+(9, 0),
+(10, 0),
+(11, 0),
+(12, 0),
+(13, 0),
+(14, 0),
+(15, 0),
+(16, 0),
+(17, 0),
+(18, 0),
+(19, 0),
+(20, 0),
+(21, 0),
+(22, 0),
+(23, 0),
+(24, 0),
+(25, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `2020`
+--
+
+CREATE TABLE IF NOT EXISTS `2020` (
+  `dateJour` int(11) NOT NULL,
+  `statut` tinyint(1) NOT NULL,
+  PRIMARY KEY (`dateJour`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `2020`
+--
+
+INSERT INTO `2020` (`dateJour`, `statut`) VALUES
+(1, 0),
+(2, 0),
+(3, 0),
+(4, 0),
+(5, 0),
+(6, 0),
+(7, 0),
+(8, 0),
+(9, 0),
+(10, 0),
+(11, 0),
+(12, 0),
+(13, 0),
+(14, 0),
+(15, 0),
+(16, 0),
+(17, 0),
+(18, 0),
+(19, 0),
+(20, 0),
+(21, 0),
+(22, 0),
+(23, 0),
+(24, 0),
+(25, 0);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
